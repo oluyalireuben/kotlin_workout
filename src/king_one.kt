b0d4e572bc04014ba9828614fce18e42f2f56f5e
Reuben Oluyali <oluyalireuben1@gmail.com>
2020-04-22 11:05:06 +0300

    first commit

---
+++ src/king_one.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var principle = 35000
+    var rate = 35.4
+    var time = 3.5
+    var simpleInterest = (principle * rate * time)/100.0
+    println(simpleInterest)
+}
